e04bf250b8df0b8f9064d5f25040c61ec9ed3049
nurrifaniherdiyanti-alt <nurrifaniherdiyanti@ummi.ac.id>
2026-04-29 21:52:23 +0700

    Initial commit: Menyiapkan struktur dasar portfolio

--- koneksi.php
+++ koneksi.php
@@ -5,9 +5,7 @@ $pass = "";
 $db   = "db_portofolio";
 
 try {
-    // Di file koneksi.php
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    // Mengatur mode error PDO ke Exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Koneksi ke database gagal: " . $e->getMessage());
